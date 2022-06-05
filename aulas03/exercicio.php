02bb9f83da1146ff62954b0cef38e14c48b86728
= <=>
2022-06-05 20:35:51 -0300

    Adicionado os exercícios feitos na aula de maneira separada

---
+++ aulas03/exercicio.php
@@ -0,0 +1,10 @@
+<?php
+
+// Criando variaveis
+ $nome_cachorro= "custelinha"; // string
+ $link_facebook = "vinny_ever@facebook.com"; // string
+ $dinheiro_atual_no_bolso = 0.5; // float
+ $fatura_da_faculdade= 500.50; //float
+ $nome_da_mae= "Neide"; // string
+ $telefone_do_amigo = "(33)9088383838"; // string
+ 
---
+++ aulas04/exercicio.php
@@ -0,0 +1,9 @@
+<?php
+
+// Exercícios constantes em um comércio eletrônico
+
+// constantes 
+$USER_NAME = "Vinny_ever"; // constante por que não altera;
+$CONTATO = "()090988558505"; // NÃO ALTERA
+$MODELO = "Regata"; // Não altera
+$NUMERO_DO_CARTAO = "89849804094"; // Não se altera um numero de cartão
